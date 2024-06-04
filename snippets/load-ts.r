270791240f317a67c36f6af57026a1861c8a3f66
Sebastian Funk <sebastian.funk@lshtm.ac.uk>
2024-06-04 22:31:57 +0100

    time series script

--- snippets/load-ts.r
+++ snippets/load-ts.r
@@ -1,7 +1,16 @@
 data(infection_times)
 df <- infection_times |>
-  mutate(infection_day = floor(infection_time))
+  transmute(infection_day = floor(infection_time))
 ## infection time series
 inf_ts <- df |>
-  count(infection_day)
+  count(infection_day, name = "infections")
+head(inf_ts)
+
+## fill data frame with zeroes for days without infection
+all_days <- expand(
+  df, infection_day = seq(min(infection_day), max(infection_day))
+)
+inf_ts <- all_days |>
+  full_join(inf_ts, by = join_by(infection_day)) |>
+  replace_na(list(infections = 0))
 head(inf_ts)
