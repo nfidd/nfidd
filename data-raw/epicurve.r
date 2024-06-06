583d89eeeb5771f814ce018ccca300091176f90f
Sebastian Funk <sebastian.funk@lshtm.ac.uk>
2024-06-06 08:59:27 +0100

    Revert "do epicurve with max"
    This reverts commit c19af42235627416a84025f5591ad365d4da20e2.
    
    There is no need for this, really

--- data-raw/epicurve.r
+++ data-raw/epicurve.r
@@ -3,20 +3,10 @@ library("usethis")
 library("dplyr")
 library("ggplot2")
 
-rgamma_max <- function(n, ..., max) {
-  ret <- rep(NA_real_, n)
-  while (anyNA(ret)) {
-    todo <- which(is.na(ret))
-    ret[todo] <- rgamma(length(todo), ...)
-    ret[ret > max] <- NA_real_
-  }
-  return(ret)
-}
-
 set.seed(12345)
 infection_times <- simulate_chains(
   index_cases = 1, offspring_dist = rpois, statistic = "size", lambda = 1.5,
-  pop = 1000, generation_time = function(n) rgamma_max(n, shape = 4, max = 14)
+  pop = 1000, generation_time = function(n) rgamma(n, shape = 4)
 ) |>
   select(infection_time = time) |>
   as.data.frame()
