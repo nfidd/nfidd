34e94530af1d70ce5a168322d2e1bc23d68ec91c
Sebastian Funk <sebastian.funk@lshtm.ac.uk>
2024-06-10 10:59:32 +0100

    restructure first 3 sessions (#94)
    * restructure first 3 sessions
    
    * re-create now missing bits

---
+++ snippets/onset-hosp.r
@@ -0,0 +1,16 @@
+### first, choose random delays
+df <- infection_times |>
+  mutate(
+    onset_time = infection_time + rgamma(n(), shape = 5, rate = 1),
+    hosp_time = onset_time + rlnorm(n(), meanlog = 1.75, sdlog = 0.5)
+  )
+### next, set 70% of the hospitalization dates to NA because only 30∞ of cases
+### are hospitalized
+df <- df |>
+  mutate(
+    hosp_time = if_else(
+      rbinom(n = n(), size = 1, p = 0.3) == 1,
+      hosp_time,
+      NA_real_
+    )
+  )
