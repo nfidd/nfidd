83bf09f34c934370b423cebe5e27d38d71434d35
kathsherratt <kathsherratt@gmail.com>
2024-06-13 21:24:48 +0100

    Update convolve-with-delay.r
    edit arg name

--- functions/convolve-with-delay.r
+++ functions/convolve-with-delay.r
@@ -4,14 +4,14 @@
 ## probabilities, corresponding to discrete indices 0, 1, 2 of the discretised
 ## delay distribution
 ## example: convolve_with_delay(c(10, 14, 10, 10), c(0.1, 0.6, 0.3))
-convolve_with_delay <- function(ts, delay) {
-  max_delay <- length(delay) - 1 ## subtract one because zero-indexed
+convolve_with_delay <- function(ts, delay_pmf) {
+  max_delay <- length(delay_pmf) - 1 ## subtract one because zero-indexed
   convolved <- vapply(seq_along(ts), \(i) {
     ## get vector of infections (14-day window)
     first_index <- max(1, i - max_delay)
     ts_segment <- ts[seq(first_index, i)]
     ## take reverse of pmf and cut if needed
-    pmf <- rev(delay)[seq_len(i - first_index + 1)]
+    pmf <- rev(delay_pmf)[seq_len(i - first_index + 1)]
     ## convolve with delay distribution
     ret <- sum(ts_segment * pmf)
     return(round(ret))
