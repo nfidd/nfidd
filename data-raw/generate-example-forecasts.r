4fb8fdbef3f5701642e83d10422c3a8309ac36c4
Sam Abbott <contact@samabbott.co.uk>
2024-06-19 21:04:27 +0100

    save rw forecast data and make visualisations

--- data-raw/generate-example-forecasts.r
+++ data-raw/generate-example-forecasts.r
@@ -4,12 +4,16 @@ library("here")
 library("cmdstanr")
 library("tidybayes")
 library("purrr")
+library("usethis")
 
 # load the model
 rw_mod <- cmdstan_model(here("stan", "estimate-inf-and-r-rw.stan"))
 
 # simulate data
 source(here::here("snippets", "simulate-onsets.r"))
+
+example_onset_df <- onset_df
+usethis::use_data(example_onset_df, overwrite = TRUE)
 
 # define a function to fit and forecast for a single date
 forecast_target_day <- function(
@@ -37,7 +41,7 @@ forecast_target_day <- function(
 
   fit <- mod$sample(
     data = data, chains = 4, parallel_chains = 4, adapt_delta = 0.95,
-    max_treedepth = 12, iter_warmup = 1000, iter_sampling = 250,
+    max_treedepth = 15, iter_warmup = 1000, iter_sampling = 250,
   )
 
   forecast <- fit |>
@@ -60,7 +64,9 @@ target_days <- onset_df |>
 target_days <- target_days[seq(1, length(target_days), 7)]
 
 
-forecasts <- target_days |>
+rw_forecasts <- target_days |>
   map_dfr(
     \(x) forecast_target_day(rw_mod, onset_df, x, horizon, gen_time_pmf, ip_pmf)
   )
+
+usethis::use_data(rw_forecasts, overwrite = TRUE)
