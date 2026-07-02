0502af0d8ce158861cfa6c3322e71ad1fbff33eb
Sebastian Funk - robot edition <242615673+sbfnk-bot@users.noreply.github.com>
2026-07-02 17:52:32 +0200

    Harmonise renewal and convolve_with_delay (#675)
    Co-authored-by: sbfnk <sebastian.funk@lshtm.ac.uk>

--- R/convolve-with-delay.r
+++ R/convolve-with-delay.r
@@ -15,19 +15,17 @@
 #' @examples
 #' convolve_with_delay(ts = c(10, 14, 10, 10), delay_pmf = c(0.1, 0.6, 0.3))
 convolve_with_delay <- function(ts, delay_pmf) {
-  max_delay <- length(delay_pmf) - 1 ## subtract one because zero-indexed
+  max_delay <- length(delay_pmf) - 1 ## delay_pmf starts at day 0
   convolved <- numeric(length(ts))
-
   for (i in seq_along(ts)) {
-    ## get vector of infections over the possible window of the delay period
+    ## window of the time series contributing to the current time point
     first_index <- max(1, i - max_delay)
-    ts_segment <- ts[seq(first_index, i)]
-    ## take reverse of pmf and cut if needed
-    pmf <- rev(delay_pmf[seq_len(i - first_index + 1)])
-    ## convolve with delay distribution
-    ret <- sum(ts_segment * pmf)
-    convolved[i] <- ret
+    len <- i - first_index + 1
+    segment <- ts[seq(first_index, i)]
+    ## reverse the (possibly truncated) pmf to align with the segment
+    pmf <- rev(delay_pmf[seq_len(len)])
+    ## convolve the time series with the delay distribution
+    convolved[i] <- sum(segment * pmf)
   }
-
   convolved
 }
--- inst/stan/functions/convolve_with_delay.stan
+++ inst/stan/functions/convolve_with_delay.stan
@@ -1,14 +1,15 @@
 array[] real convolve_with_delay(array[] real ts, array[] real delay) {
-  // length of time series
   int n = num_elements(ts);
-  int max_delay = num_elements(delay) - 1;
-  array[n] real ret;
+  int max_delay = num_elements(delay) - 1; // delay starts at day 0
+  array[n] real convolved;
   for (i in 1:n) {
+    // window of the time series contributing to the current time point
     int first_index = max(1, i - max_delay);
     int len = i - first_index + 1;
-    array[len] real pmf = reverse(delay)[1:len];
-    array[i - first_index + 1] real ts_segment = ts[first_index:i];
-    ret[i] = dot_product(ts_segment, pmf);
+    array[len] real segment = ts[first_index:i];
+    // reverse the (possibly truncated) pmf to align with the segment
+    array[len] real pmf = reverse(delay[1:len]);
+    convolved[i] = dot_product(segment, pmf);
   }
-  return(ret);
+  return convolved;
 }
